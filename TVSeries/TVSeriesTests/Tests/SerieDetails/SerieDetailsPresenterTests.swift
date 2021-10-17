8d242cad13e7b1ff82b5286f9d64cd3a7b158a5f
anacamargos <alcvlara@sga.pucminas.br>
2021-10-17 12:51:40 -0300

    Creating missing tests

---
+++ TVSeries/TVSeriesTests/Tests/SerieDetails/SerieDetailsPresenterTests.swift
@@ -0,0 +1,66 @@
+//
+//  SerieDetailsPresenterTests.swift
+//  TVSeriesTests
+//
+//  Created by Ana Leticia Camargos on 17/10/21.
+//
+
+import XCTest
+@testable import TVSeries
+
+final class SerieDetailsPresenterTests: XCTestCase {
+    
+    func test_presentSerieDetails_shouldCallCorrectMethodInViewController() {
+        // Given
+        let viewControllerSpy = SerieDetailsDisplayLogicSpy()
+        let sut = makeSUT(viewController: viewControllerSpy)
+        let expectedViewData = SerieDetails.Serie.testMock
+
+        // When
+        sut.presentSerieDetails(.mock)
+
+        // Then
+        XCTAssertEqual(String(describing: viewControllerSpy.displaySerieDetailsViewDataPassedViewStates), String(describing: [expectedViewData]))
+    }
+
+    // MARK: - Test Helpers
+
+    private func makeSUT(
+        viewController: SerieDetailsDisplayLogic = SerieDetailsDisplayLogicDummy(),
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> SerieDetailsPresenter {
+        let sut = SerieDetailsPresenter()
+        sut.viewController = viewController
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(viewController, file: file, line: line)
+        return sut
+    }
+
+}
+
+final class SerieDetailsDisplayLogicDummy: SerieDetailsDisplayLogic {
+    func displaySerieDetailsViewData(_ viewData: SerieDetails.Serie) {}
+    func displayEpisodesViewState(_ viewState: SerieDetails.ViewState) {}
+}
+
+final class SerieDetailsDisplayLogicSpy: SerieDetailsDisplayLogic {
+    
+    private(set) var displaySerieDetailsViewDataPassedViewStates = [SerieDetails.Serie]()
+    
+    func displaySerieDetailsViewData(_ viewData: SerieDetails.Serie) {
+        displaySerieDetailsViewDataPassedViewStates.append(viewData)
+    }
+    
+    private(set) var displayEpisodesViewStatePassedViewStates = [SerieDetails.ViewState]()
+    
+    func displayEpisodesViewState(_ viewState: SerieDetails.ViewState) {
+        displayEpisodesViewStatePassedViewStates.append(viewState)
+    }
+}
+
+extension SerieDetails.Serie {
+    static var testMock: SerieDetails.Serie {
+        .init(name: "Once Upon a Time", summary: "Test 1 Test 2 Test 3", imageURL: nil, schedule: "Schedule: Fridays", genres: "Genres: Drama")
+    }
+}
